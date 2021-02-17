6156e7ca20687a5356f53f54170d745a6df7edb4
Yoni491 <54304056+Yoni491@users.noreply.github.com>
2021-02-17 20:50:25 +0200

    continue from here 2

---
+++ Game1/monogame1/GameServer/Models/NetworkPlayer.cs
@@ -0,0 +1,88 @@
+﻿//using Microsoft.Xna.Framework;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Sockets;
+
+//namespace GameServer
+//{
+//    public class NetworkPlayer
+//    {
+//        static int playerNum = 0;
+
+//        private int playerNum1;
+
+//        public Gun _gun;
+
+//        private float _speed = 2f;
+
+//        public Vector2 _velocity;
+
+//        private Vector2 _position;
+
+//        public HealthManager _health;
+
+//        private Vector2 _looking_direction;
+
+//        public Socket _socket;
+
+//        public PacketLong_Server _longPacket;
+
+//        public PacketShort_Server _shortPacket; 
+//        public Vector2 Position { get => _position; set => _position = value; }
+//        public int PlayerNum { get => playerNum1; set => playerNum1 = value; }
+
+//        private int _animationNum;
+//        private int _gunNum;
+
+//        public NetworkPlayer(Vector2 position, int health, Socket socket, List<NetworkPlayer> players)
+//        {
+//            _position = position;
+//            _health = new HealthManager(health, position + new Vector2(8, 10));
+//            _velocity = Vector2.Zero;
+//            PlayerNum = playerNum++;
+//            _gun = new Gun(position, 3);
+//            _socket = socket;
+//            _shortPacket = new PacketShort_Server(players);
+//            _longPacket = new PacketLong_Server(players, this);
+//        }
+//        public void EquipGun(Gun gun)
+//        {
+//            _gun = gun;
+//        }
+//        public void Update(GameTime gameTime, List<Simple_Enemy> enemies)
+//        {
+//            _position += _velocity;
+
+//            if (_gun != null)
+//            {
+//                _gun.Update(gameTime, enemies, _looking_direction);
+//            }
+//            //_health._position = _position + new Vector2(8, 10);
+//        }
+//        public void UpdatePacketShort(PacketStructure packet)
+//        {
+//            packet.WriteInt(PlayerNum);
+//            packet.WriteVector2(Position);
+//            packet.WriteInt(_health._health_left);
+//            packet.WriteInt(_health._total_health);
+//            packet.WriteVector2(_velocity);
+//            packet.WriteVector2(_looking_direction);
+//            packet.WriteInt(_animationNum);
+//            packet.WriteInt(_gunNum);
+//            packet.WriteInt(_gun._bullets.Count());
+//            _gun.UpdatePacketShort(packet);
+//        }
+//        public void ReadPacketShort(PacketStructure packet)
+//        {
+//            Position = packet.ReadVector2();
+//            _health._health_left = packet.ReadInt();
+//            _health._total_health = packet.ReadInt();
+//            _velocity = packet.ReadVector2();
+//            _looking_direction = packet.ReadVector2();
+//            _animationNum = packet.ReadInt();
+//            _gunNum = packet.ReadInt();
+//            _gun.ReadPacketShort(packet);
+//        }
+//    }
+//}
+
---
+++ Game1/monogame1/GameServer/Network/NetworkManagerServer.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    public class NetworkManagerServer
+    {
+        float _timer_short = 0;
+        float _timer_long = 0;
+        static List<Socket> _socket_list;
+        private List<NetworkPlayer> _players;
+        Socket _socketServer;
+        private List<byte> _bufferList;
+        int packetType;
+        List<PacketHandlerServer> _packetHandlers;
+        int numOfPlayer = 0;
+        int addPlayers = 0;
+        List<Socket> _socketToAdd;
+
+        public NetworkManagerServer(List<Socket> socket_list, List<NetworkPlayer> players)
+        {
+            _socket_list = socket_list;
+            _players = players;
+            _packetHandlers = new List<PacketHandlerServer>();
+            _socketToAdd = new List<Socket>();
+            _bufferList = new List<Byte>();
+        }
+        public void Update(GameTime gameTime)
+        {
+            int tempPlayers = addPlayers;
+            for (int i = 0; i < tempPlayers; i++)
+            {
+                Socket socket= _socketToAdd[0];
+                _socket_list.Add(socket);
+                _socketToAdd.RemoveAt(0);
+                byte[] buffer = new byte[10000];
+                NetworkPlayer player = new NetworkPlayer(Vector2.Zero, 100,socket,_players);
+                _players.Add(player);
+                PacketHandlerServer packetHandler = new PacketHandlerServer(_players, player);
+                _packetHandlers.Add(packetHandler);
+                numOfPlayer++;
+                PacketStructure packet = new PacketStructure();
+                packet.UpdateType(3);
+                packet.WriteInt(player.PlayerNum);
+                socket.Send(packet.Data());
+                Receive(socket, packetHandler,buffer);
+            }
+            addPlayers -= tempPlayers;
+            _timer_short += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (_timer_short >= 0.05f)
+            {
+                _timer_short = 0;
+                foreach (var player in _players)
+                {
+                    player._shortPacket.UpdatePacket();
+                    if (player._socket.Connected)
+                    {
+                        player._socket.Send(player._shortPacket.Data());
+                        packetType = player._shortPacket.ReadUShort();
+                        if (packetType != 0)
+                            Console.WriteLine("server: packet left Length: {0} | type: {1}", packetType, player._shortPacket.ReadUShort());
+                    }
+                }
+                foreach (var player in _players)
+                {
+                    player._gun._bullets.Clear();
+                }
+            }
+            _timer_long += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (_timer_long >= 1.5f)
+            {
+                _timer_long = 0;
+                foreach (var player in _players)
+                {
+                    if (player._socket.Connected)
+                    {
+                        player._longPacket.UpdatePacket();
+                        player._socket.Send(player._longPacket.Data());
+                        packetType = player._longPacket.ReadUShort();
+                        if (packetType != 0)
+                            Console.WriteLine("server: packet left Length: {0} | type: {1}", packetType, player._longPacket.ReadUShort());
+                    }
+                }
+            }
+            for (int i = 0; i < numOfPlayer; i++)
+            {
+                _packetHandlers[i].Update();
+            }
+
+        }
+        public void Initialize_connection()
+        {
+            _socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socketServer.Bind(new IPEndPoint(0, 1994));
+            _socketServer.Listen(0);
+            Accept();
+
+        }
+        private void Accept()
+        {
+            _socketServer.BeginAccept(AcceptCallBack, null);
+        }
+        private void AcceptCallBack(IAsyncResult result)
+        {
+            Socket client_socket = _socketServer.EndAccept(result);
+            _socketToAdd.Add(client_socket);
+            addPlayers++;
+            Accept();
+            
+        }
+        private void Receive(Socket client_socket, PacketHandlerServer packetHandlerServer, byte[] buffer)
+        {
+            client_socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceivedCallBack, Tuple.Create(client_socket, packetHandlerServer, buffer));
+        }
+        private void ReceivedCallBack(IAsyncResult result)
+        {
+            Tuple<Socket,  PacketHandlerServer, byte[]> state = (Tuple<Socket, PacketHandlerServer, byte[]>)result.AsyncState;
+            Socket client_socket = state.Item1;
+            PacketHandlerServer packetHandlerServer = state.Item2;
+            byte[] buffer = state.Item3;
+            int buffer_size = client_socket.EndReceive(result);
+            packetHandlerServer.Handle(buffer);
+            Receive(client_socket, packetHandlerServer,buffer);
+        }
+    }
+}
